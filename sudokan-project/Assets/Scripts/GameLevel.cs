7e597092c331130a725f9620aeb7d2db145ff2d9
Trương Công Quốc Cường <59004156+TeoLeoGit@users.noreply.github.com>
2024-10-14 12:16:29 +0700

    Add project.

---
+++ sudokan-project/Assets/Scripts/GameLevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLevel : MonoBehaviour
+{
+    private int[][] grid;
+
+    public void GenGrid()
+    {
+
+    }
+
+    public void UpdateGride()
+    {
+
+    }    
+}
